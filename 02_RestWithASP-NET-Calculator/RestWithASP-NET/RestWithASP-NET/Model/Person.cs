ee4f1f7c7111ae362cfaec53d169c5c9450aeb0a
Tecnimex Ouro Branco <gabrielrsbarbosa@gmail.com>
2024-07-29 22:59:44 -0300

    update 28-07

--- 02_RestWithASP-NET-Calculator/RestWithASP-NET/RestWithASP-NET/Model/Person.cs
+++ 02_RestWithASP-NET-Calculator/RestWithASP-NET/RestWithASP-NET/Model/Person.cs
@@ -1,5 +1,9 @@
-﻿namespace RestWithASP_NET.Model
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RestWithASP_NET.Model;
+
     public class Person
     {
         public long Id { get; set; }
