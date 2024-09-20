d316a65f4a39e422a5faee2b81512d70dcd84253
Gabriel Barbosa <gabrielrsbarbosa@outlook.com>
2024-09-20 10:57:34 -0300

    S-19 autentication

---
+++ 03_RestWithASP-NET-S-19/RestWithASP-NET/RestWithASP-NET/Controllers/PersonController.cs
@@ -0,0 +1,163 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASP_NET.Business;
+using RestWithASP_NET.Data.VO;
+using RestWithASP_NET.Hypermedia.Filters;
+using RestWithASP_NET.Model;
+using RestWithASP_NET.Repository;
+
+namespace RestWithASP_NET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<PersonVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+           
+            return Ok(_personBusiness.FindAll());
+        }
+        
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Delete(long id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+
+
+        /* calculater
+                [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+                public IActionResult Subtraction(string firstNumber, string secondNumber)
+                {
+                    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                    {
+                        var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                        return Ok(sum.ToString());
+                    }
+                    return BadRequest("Ivaladi Input");
+                }
+
+                [HttpGet("multiplicstion/{firstNumber}/{secondNumber}")]
+                public IActionResult Multiplication(string firstNumber, string secondNumber)
+                {
+                    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                    {
+                        var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                        return Ok(sum.ToString());
+                    }
+                    return BadRequest("Ivaladi Input");
+                }
+
+                [HttpGet("division/{firstNumber}/{secondNumber}")]
+                public IActionResult Division(string firstNumber, string secondNumber)
+                {
+                    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                    {
+                        var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                        return Ok(sum.ToString());
+                    }
+                    return BadRequest("Ivaladi Input");
+                }
+
+                [HttpGet("mean/{firstNumber}/{secondNumber}")]
+                public IActionResult Mean(string firstNumber, string secondNumber)
+                {
+                    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                    {
+                        var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                        return Ok(sum.ToString());
+                    }
+                    return BadRequest("Ivaladi Input");
+                }
+
+                [HttpGet("squareroot/{firstNumber}")]
+                public IActionResult SquareRoot(string firstNumber)
+                {
+                    if (IsNumeric(firstNumber) )
+                    {
+                        var squareRoot = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+                        return Ok(squareRoot.ToString());
+                    }
+                    return BadRequest("Ivaladi Input");
+                }
+
+                private decimal ConvertToDecimal(string strNumber)
+                {
+                    decimal decimalValue;
+                    if (decimal.TryParse(strNumber, out decimalValue)) 
+                    {
+                        return decimalValue;
+                    }
+
+                    return 0 ;
+                }
+
+                private bool IsNumeric(string strNumber)
+                {
+                    double number;
+                    bool isNumeric = double.TryParse(
+                        strNumber,
+                        System.Globalization.NumberStyles.Any,
+                        System.Globalization.NumberFormatInfo.InvariantInfo,
+                        out number);
+                    return isNumeric;
+                }*/
+    }
+}
---
+++ 03_RestWithASP-NET-S-19/RestWithASP-NET/RestWithASP-NET/Data/VO/PersonVO.cs
@@ -0,0 +1,21 @@
+﻿using RestWithASP_NET.Hypermedia;
+using RestWithASP_NET.Hypermedia.Abstract;
+using System.Text.Json.Serialization;
+
+namespace RestWithASP_NET.Data.VO;
+
+public class PersonVO : ISupportsHyperMedia
+{
+    // [JsonPropertyName("code")]--- para serializar a costumização 
+    //  [JsonIgnore]--- para ignorar o item abaixo
+    public long Id { get; set; }
+   
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+   
+    public string Addres { get; set; }
+   
+    public string Gender { get; set; }
+    public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+}
