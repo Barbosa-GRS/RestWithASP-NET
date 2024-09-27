a545a8526e6cafee22a9dbfa29c8cea8a9f8597e
Gabriel Barbosa <gabrielrsbarbosa@outlook.com>
2024-09-26 22:09:38 -0300

    finish Autentication

---
+++ 02_RestWithASP-NET-Verbo PATCH/RestWithASP-NET/RestWithASP-NET/Hypermedia/ContentResponseEnricher.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Routing;
+using RestWithASP_NET.Hypermedia.Abstract;
+using System.Collections.Concurrent;
+
+namespace RestWithASP_NET.Hypermedia
+{
+    public abstract class ContentResponseEnricher<T> : IResponseEnricher where T : ISupportsHyperMedia
+    {
+        public ContentResponseEnricher()
+        {
+
+        }
+        public bool CanEnricher(Type contentType)
+        {
+            return contentType == typeof(T) || contentType == typeof(List<T>);
+        }
+        protected abstract Task EnricherModel( T content, IUrlHelper urlHelper);
+        bool IResponseEnricher.CanEnricher(ResultExecutingContext response)
+        {
+            if (response.Result is OkObjectResult okObjectResult)
+            {
+                return CanEnricher(okObjectResult.Value.GetType());
+
+            }
+            return false;
+        }
+
+        public async Task Enricher(ResultExecutingContext response)
+        {
+            var urlHelper = new UrlHelperFactory().GetUrlHelper(response);
+            if (response.Result is OkObjectResult okObjectResult)
+            {
+                if (okObjectResult.Value is T model)
+                {
+                    await EnricherModel(model, urlHelper);
+                } 
+                else if (okObjectResult.Value is List<T> collection)
+                {
+                    ConcurrentBag<T> bag = new ConcurrentBag<T>(collection);
+                    Parallel.ForEach(bag, (element) =>
+                    {
+                        EnricherModel(element, urlHelper);
+                    });
+                }
+            }
+            await Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ 02_RestWithASP-NET-Verbo PATCH/RestWithASP-NET/RestWithASP-NET/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using RestWithASP_NET.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RestWithASP_NET.Model;
+
+[Table("person")]
+public class Person : BaseEntity
+{
+    
+    [Column("first_name")]
+    public string FirstName { get; set; }
+    
+    [Column("last_name")]
+    public string LastName { get; set; }
+    
+    [Column("address")]
+    public string Addres { get; set; }
+    
+    [Column("gender")]
+    public string Gender { get; set; }
+}
